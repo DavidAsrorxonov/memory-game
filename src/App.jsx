605820f2d7cd33b43dfb84a14db20587f143a145
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-15 16:35:40 +0900

    add a function that handles the card click and flips the card according to the id; make the styles in the card file dynamic so that the style of the pressed card changes

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,30 @@ function App() {
     initializeGame();
   }, []);
 
+  const handleCardClick = (card) => {
+    if (card.isFlipped || card.isMatched) {
+      return;
+    }
+
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return { ...c, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards);
+  };
+
   return (
     <>
       <div className="app">
         <GameHeader score={3} moves={10} />
 
         <div className="cards-grid">
-          {cardValues.map((card, index) => (
-            <Card card={card} key={index} />
+          {cards.map((card, index) => (
+            <Card card={card} key={index} onClick={handleCardClick} />
           ))}
         </div>
       </div>
