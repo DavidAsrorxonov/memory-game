519bf04ab05325cc6808be9e9c79e5bd4501d47e
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-15 17:37:16 +0900

    everything is completed

--- src/components/WinMessage.jsx
+++ src/components/WinMessage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const WinMessage = ({ moves }) => {
   return (
     <div className="win-message">
-      <h2>Congratulations!</h2>
+      <h2>Congratulations!!!</h2>
       <p>You completed the game in {moves} moves!</p>
     </div>
   );
